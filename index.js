9c7576f25996fcafda4f7d08697ed30999d58be2
Muhammad Faizan <muhammadfaizan027915@gmail.com>
2023-11-18 22:51:30 +0500

    [Done] Routing in node js.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send({
-        message: "Welcome to the application"
-    })
-})
+const users = [
+  {
+    userId: 1,
+    name: "Muhammad Faizan",
+  },
 
-const Port = 5000
+  {
+    userId: 2,
+    name: "Muhammad Ali",
+  },
+
+  {
+    userId: 3,
+    name: "Ali Abdullah",
+  },
+];
+
+app.get("/", (req, res) => {
+  return res.send({
+    message: "Welcome to the application",
+  });
+});
+
+app.post("/about", (req, res) => {
+  return res.send({
+    name: "Muhammad Faizan",
+    email: "muhammadfaizan027915@gmail.com",
+  });
+});
+
+app.get("/users", (req, res) => {
+  return res.send(users);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  const foundUser = users.find((user) => user.userId == userId);
+
+  if (foundUser) res.send(foundUser);
+
+  res.send({
+    message: "Required user not found",
+  });
+});
+
+const Port = 5000;
 app.listen(Port, () => console.log("Server started...." + Port));
